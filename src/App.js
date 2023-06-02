726b58a59172c1ed56481a5112a54678cf7285b9
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-06-02 12:59:25 +0700

    Add home navigation and add some animation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import { BrowserRouter } from "react-router-dom";
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>Foodlicious</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -14,5 +21,24 @@ const App = () => {
     </div>
   );
 };
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.75rem;
+  font-weight: 600;
+  margin: 0rem 0.5rem;
+  font-family: "Great Vibes", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
